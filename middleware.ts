21620410a726812b3b5217854a5d224b0c8ed78d
QuangTran013 <113117168+daVinc3nt@users.noreply.github.com>
2024-02-27 14:07:43 +0700

    test

--- middleware.ts
+++ middleware.ts
@@ -8,21 +8,20 @@ const authRoutes = "/log";
 
 export function middleware(request: NextRequest) {
   const currentUser = request.cookies.get("connect.sid")?.value;
-  console.log(request.nextUrl.pathname.startsWith(protectedRoutes))
   if (
     request.nextUrl.pathname.startsWith(protectedRoutes) &&
     (!currentUser || Date.now() > JSON.parse(currentUser).expiredAt)
   ) {
-    console.log("if 1")
     request.cookies.delete("currentUser");
     const response = NextResponse.redirect(new URL(`${request.nextUrl.locale === "en" ? "en":""}/log`, request.url));
     response.cookies.delete("currentUser");
     return response;
   }
-  console.log(currentUser)
+  console.log(request.nextUrl.pathname.startsWith(authRoutes) && currentUser);
   if (request.nextUrl.pathname.startsWith(authRoutes) && currentUser) {
-    console.log("if 2")
     return NextResponse.redirect(new URL("/dashboard", request.url));
-
   }
 }
+export const config = {
+    matcher: ['/:path*'],
+  }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect} from "react";
+import { useState, useEffect } from "react";
 import Wrapper from "@/components/LayoutWrapper";
 import { useRouter } from "next/router";
 import { IntlProvider } from "react-intl";
@@ -13,6 +13,40 @@ import 'aos/dist/aos.css'
 const googleMapsLibraries: Libraries = ["places"];
 
 function MyApp({ Component, pageProps }: AppProps) {
+  function Loading() {
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+      const handleStart = (url) => url !== router.asPath && setLoading(true);
+      const handleComplete = (url) =>
+        url === router.asPath && setLoading(false);
+
+      router.events.on("routeChangeStart", handleStart);
+      router.events.on("routeChangeComplete", handleComplete);
+      router.events.on("routeChangeError", handleComplete);
+
+      // Clean up the event listeners when the component unmounts
+      return () => {
+        router.events.off("routeChangeStart", handleStart);
+        router.events.off("routeChangeComplete", handleComplete);
+        router.events.off("routeChangeError", handleComplete);
+      };
+    }, [router]); // Add router to the dependency array
+
+    return (
+      loading && (
+        <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-screen h-screen bg-white opacity-75">
+          <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
+        </div>
+      )
+    );
+  }
   const { locale } = useRouter();
   const messages = {
     vi,
@@ -36,6 +70,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           googleMapsApiKey={"AIzaSyDQ0pDRDKSyAO4lm10ttEXa2_uoZmWQzHc"}
         >
           <Wrapper>
+            <Loading/>
             <Component {...pageProps} />
           </Wrapper>
         </LoadScript>
